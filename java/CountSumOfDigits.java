c714ccfcbabef34b99a73ba6933f1e7a3323ee01
Birobrata Deb <birobratadeb@yahoo.co.in>
2020-11-29 14:27:26 +0000

    updated code
    added def

---
+++ java/CountSumOfDigits.java
@@ -0,0 +1,18 @@
+package sdet_javaProg;
+
+public class CountSumOfDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 1234;
+		int sum = 0;
+		while(num>0) {
+			sum = sum + num%10;
+			num = num/10;
+		}
+		System.out.println("Sum of digits: "+sum);		
+	}
+
+}
+
