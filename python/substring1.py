dec2973f4c296d2fc06a7161c91fcc1a9b727acc
Birobrata Deb <birobratadeb@yahoo.co.in>
2020-11-29 12:22:04 +0000

    updated code
    added def

--- python/substring1.py
+++ python/substring1.py
@@ -1,8 +1,19 @@
-def subString(str, len):
-    for i in range(len): # range(len) --> 0,1,2,3
-        count = 0;
-        for len in range(i+1, len+1):  # --> i: 0->1,  1-> 2, 2->3, 3->4   < len+1=5, means upto 4
-            print(s[i:len]);
-            count=count+1;
+# Python program to print all possible substrings of a given string
+  
+# Function to print all sub strings
+def subString(s, n):
+    # Pick starting point in outer loop and lengths of different strings for a given starting point
+    for i in range(n):
+        for len in range(i+1,n+1):
+            print(s[i: len]);
+ 
+# Python3 program to count number of substrings of a string 
+
+def countNonEmptySubstr(str):
+    n = len(str);
+    return int(n * (n + 1) / 2);
+
+# Driver program to test above function
 s = "abcd";
-subString(s, len(s)); # len(s) = 4
+subString(s,len(s));
+print(countNonEmptySubstr(s));
