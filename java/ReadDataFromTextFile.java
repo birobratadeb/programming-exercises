37276a97021d5ed3c39c9d0580c7f9277f31f498
Birobrata Deb <birobratadeb@yahoo.co.in>
2020-12-10 22:03:02 +0000

    How To Read Data From Text File
    Added description

---
+++ java/ReadDataFromTextFile.java
@@ -0,0 +1,60 @@
+package sdet_javaProg;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+// Approach 1: Using FileReader and BufferedReader
+public class ReadDataFromTextFile {
+
+	public static void main(String[] args) throws IOException
+	
+	{
+		
+		FileReader fr = new FileReader("C:\\Users\\debbi\\eclipse-workspace\\sdet_javaProg\\src\\sdet_javaProg\\Test.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		String str;
+		while((str = br.readLine())!= null)
+		{
+			System.out.println(str);
+		}
+	}
+}
+*/
+
+/*
+//Approach 2: Using Scanner and File and loop
+public class ReadDataFromTextFile {
+
+	public static void main(String[] args) throws IOException
+	
+	{
+		File file = new File("C:\\Users\\debbi\\eclipse-workspace\\sdet_javaProg\\src\\sdet_javaProg\\Test.txt");
+		Scanner sc = new Scanner(file);
+		
+		while(sc.hasNextLine())
+		{
+			System.out.println(sc.nextLine());
+		}				
+	}
+}
+
+*/
+
+//Approach 3: Using Scanner, File, regex and without loop
+public class ReadDataFromTextFile {
+
+	public static void main(String[] args) throws IOException
+	
+	{
+		File file = new File("C:\\Users\\debbi\\eclipse-workspace\\sdet_javaProg\\src\\sdet_javaProg\\Test.txt");
+		Scanner sc = new Scanner(file);
+		sc.useDelimiter("//z");
+		System.out.println(sc.next());			
+	}
+}
