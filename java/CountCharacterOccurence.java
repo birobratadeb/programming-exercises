f4c093817c48a75224d23c58df5906be03481748
Birobrata Deb <birobratadeb@yahoo.co.in>
2020-12-10 07:40:55 +0000

    How To Count Occurrences of a Character in a String
    Added description

---
+++ java/CountCharacterOccurence.java
@@ -0,0 +1,14 @@
+package sdet_javaProg;
+
+public class CountCharacterOccurence {
+
+	public static void main(String[] args) {
+		
+		String s= "Java programming Java oops";
+		int InitialLength = s.length();		
+		int LengthAfterRemoval = s.replaceAll("J", "").length();
+		int count = InitialLength - LengthAfterRemoval;
+		
+		System.out.println("Number of occurences of a: "+count);	
+	}
+}
