61191476d73c18bfdc1ad92e7ac500fb2541e127
Birobrata Deb <birobratadeb@yahoo.co.in>
2021-02-03 00:14:52 +0000

    fibonacci series
    Multiple approaches-csestack.org

---
+++ c/fibonacci.c
@@ -0,0 +1,47 @@
+
+/*
+#include<stdio.h>
+int fibo(int);
+int main()
+{
+    int t, m, n=0;
+    printf("Fibonacci example using recursion\n");
+    printf("Enter number of terms:\n");
+    scanf("%d",&t);
+    printf"Fibonacci series for the %d terms is:\n",t);
+    for(m=1; m<=t; m++)
+    {
+        printf("%d\n", fibo(n));
+        n++;
+    }
+    return 0;
+}
+int fibo(int n)
+{
+
+    if(n==0 || n==1)
+        return n;
+    else
+        return(fib(n-1)+fibo(n-2)) ;
+}
+
+
+*/
+
+
+
+
+
+/*
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+*/
+
+
